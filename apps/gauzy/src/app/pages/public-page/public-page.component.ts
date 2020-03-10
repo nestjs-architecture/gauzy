9e6b6322143a0a4b81291583f3bd1b2139cae0bc
Robin Hossain <robinsabbir@gmail.com>
2020-03-10 17:44:22 +0600

    feat(ui): public page components

---
+++ apps/gauzy/src/app/pages/public-page/public-page.component.ts
@@ -0,0 +1,162 @@
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Store } from '../../@core/services/store.service';
+import { OrganizationsMutationComponent } from '../../@shared/organizations/organizations-mutation/organizations-mutation.component';
+import { first } from 'rxjs/operators';
+import { NbDialogService, NbToastrService } from '@nebular/theme';
+import { OrganizationsService } from '../../@core/services/organizations.service';
+import { Router } from '@angular/router';
+import { EmployeesService } from '../../@core/services';
+import { TranslateService } from '@ngx-translate/core';
+import { ErrorHandlingService } from '../../@core/services/error-handling.service';
+import { TranslationBaseComponent } from '../../@shared/language-base/translation-base.component';
+import { Organization } from '@gauzy/models';
+import { LocalDataSource } from 'ng2-smart-table';
+import { OrganizationsFullnameComponent } from '../organizations/table-components/organizations-fullname/organizations-fullname.component';
+import { OrganizationsEmployeesComponent } from '../organizations/table-components/organizations-employees/organizations-employees.component';
+import { OrganizationsCurrencyComponent } from '../organizations/table-components/organizations-currency/organizations-currency.component';
+import { OrganizationsStatusComponent } from '../organizations/table-components/organizations-status/organizations-status.component';
+import { Subject } from 'rxjs';
+
+interface SelectedRow {
+	data: Organization;
+	isSelected: boolean;
+	selected: Organization[];
+	source: LocalDataSource;
+}
+
+@Component({
+	selector: 'ngx-public-page',
+	templateUrl: './public-page.component.html',
+	styleUrls: ['./public-page.component.scss']
+})
+export class PublicPageComponent extends TranslationBaseComponent
+	implements OnInit, OnDestroy {
+	constructor(
+		private organizationsService: OrganizationsService,
+		private toastrService: NbToastrService,
+		private dialogService: NbDialogService,
+		private router: Router,
+		private employeesService: EmployeesService,
+		readonly translateService: TranslateService,
+		private errorHandler: ErrorHandlingService,
+		private store: Store
+	) {
+		super(translateService);
+	}
+
+	private _ngDestroy$ = new Subject<void>();
+
+	@ViewChild('settingsTable', { static: false }) settingsTable;
+
+	settingsSmartTable: object;
+	selectedOrganization: Organization;
+	smartTableSource = new LocalDataSource();
+
+	loading = true;
+	hasEditPermission = false;
+	hasEditExpensesPermission = false;
+
+	loadSettingsSmartTable() {
+		this.settingsSmartTable = {
+			actions: false,
+			columns: {
+				name: {
+					title: this.getTranslation('SM_TABLE.CLIENT_NAME'),
+					type: 'custom',
+					renderComponent: OrganizationsFullnameComponent
+				},
+				totalEmployees: {
+					title: this.getTranslation('SM_TABLE.EMPLOYEES'),
+					type: 'custom',
+					width: '200px',
+					class: 'text-center',
+					filter: false,
+					renderComponent: OrganizationsEmployeesComponent
+				},
+				currency: {
+					title: this.getTranslation('SM_TABLE.CURRENCY'),
+					type: 'custom',
+					class: 'text-center',
+					width: '200px',
+					renderComponent: OrganizationsCurrencyComponent
+				},
+				status: {
+					title: this.getTranslation('SM_TABLE.STATUS'),
+					type: 'custom',
+					class: 'text-center',
+					width: '200px',
+					filter: false,
+					renderComponent: OrganizationsStatusComponent
+				}
+			},
+			pager: {
+				display: true,
+				perPage: 8
+			}
+		};
+	}
+
+	ngOnInit() {
+		this._loadSmartTable();
+		this.loadSettingsSmartTable();
+		this._applyTranslationOnSmartTable();
+	}
+
+	_applyTranslationOnSmartTable() {
+		this.translateService.onLangChange.subscribe(() => {
+			this.loadSettingsSmartTable();
+		});
+	}
+
+	async addPublicPage() {
+		const result = await this.dialogService
+			.open(OrganizationsMutationComponent)
+			.onClose.pipe(first())
+			.toPromise();
+
+		if (result) {
+			try {
+				await this.organizationsService.create(result);
+				this.toastrService.primary(
+					this.getTranslation(
+						'NOTES.ORGANIZATIONS.ADD_NEW_ORGANIZATION',
+						{
+							name: result.name
+						}
+					),
+					this.getTranslation('TOASTR.TITLE.SUCCESS')
+				);
+				this._loadSmartTable();
+			} catch (error) {
+				this.errorHandler.handleError(error);
+			}
+		}
+	}
+
+	private async _loadSmartTable() {
+		try {
+			const { items } = await this.organizationsService.getAll();
+
+			for (const org of items) {
+				const data = await this.employeesService
+					.getAll([], { organization: { id: org.id } })
+					.pipe(first())
+					.toPromise();
+
+				const activeEmployees = data.items.filter((i) => i.isActive);
+				org.totalEmployees = activeEmployees.length;
+			}
+
+			this.smartTableSource.load(items);
+			if (this.settingsTable) {
+				this.settingsTable.grid.dataSet.willSelect = 'false';
+			}
+		} catch (error) {
+			this.errorHandler.handleError(error);
+		}
+
+		this.loading = false;
+	}
+
+	ngOnDestroy() {}
+}
